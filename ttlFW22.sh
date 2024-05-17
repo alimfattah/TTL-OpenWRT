bbb25b1af1ae86b2551e03848086b2494088d6bc
Fattahul Alim Muhamad Abdi <140358363+alimfattah@users.noreply.github.com>
2024-05-17 19:05:53 +0700

    Create ttlFW22.sh

---
+++ ttlFW22.sh
@@ -0,0 +1,12 @@
+
+echo "chain mangle_postrouting_ttl65 {
+  type filter hook postrouting priority 300; policy accept;
+  counter ip ttl set 65
+}
+
+chain mangle_prerouting_ttl65 {
+  type filter hook prerouting priority 300; policy accept;
+  counter ip ttl set 65
+}" > /etc/nftables.d/10-custom-filter-chains.nft
+service firewall restart
+
